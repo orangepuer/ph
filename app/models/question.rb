6d111a6c4d26712da3db73cc7e6bc8d92e74c3b6
orangepuer <rkhpuer@gmail.com>
2019-02-06 20:25:36 +0300

    Add created_before method to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ApplicationRecord
   validates :title, :body, presence: true
+
+  def created_before
+    @numbers_of_day = (Time.now - self.created_at).to_f/60/60/24
+    if @numbers_of_day >= 1
+      "опубликовано около #{ @numbers_of_day.to_i } дней назад"
+    else
+      "опубликовано менее #{ ((@numbers_of_day - @numbers_of_day.to_i) * 24).to_i + 1 } часов назад"
+    end
+  end
 end
