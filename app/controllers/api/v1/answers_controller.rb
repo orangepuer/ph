464c4e80b36a5a9f84af00af73a7f7d262ad036a
orangepuer <rkhpuer@gmail.com>
2019-04-10 10:31:55 +0300

    Add API to get answer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::AnswersController < Api::V1::BaseController
     @question = Question.find(params[:question_id])
     render json: @question.answers
   end
+
+  def show
+    @answer = Answer.find(params[:id])
+    render json: @answer
+  end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -47,4 +47,79 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'GET #show' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answer) { create(:answer, question: question, user: user) }
+
+    context 'unauthorized' do
+      it 'return 401 status if there is no access_token' do
+        get "/api/v1/answers/#{answer.id}", headers: headers
+
+        expect(response.status).to eq 401
+      end
+
+      it 'return 401 status if access_token invalid' do
+        get "/api/v1/answers/#{answer.id}", params: {access_token: '123456'}, headers: headers
+
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:comments) { create_list(:comment, 5, commentable: answer, user: user) }
+      let!(:attachments) { create_list(:attachment, 4, attachmentable: answer) }
+      let(:access_token) { create(:access_token) }
+      let(:answer_response) { json['answer'] }
+
+      before { get "/api/v1/answers/#{answer.id}", params: {access_token: access_token.token}, headers: headers }
+
+      it 'return 200 status' do
+        expect(response).to be_successful
+      end
+
+      %w(id body created_at updated_at).each do |attribute|
+        it "answer object contains #{attribute}" do
+          expect(answer_response[attribute]).to eq answer.send(attribute).as_json
+        end
+      end
+
+      context 'comments' do
+        let(:comment) { comments.first }
+        let(:comments_response) { answer_response['comments'] }
+        let(:comment_response) { comments_response.last }
+
+        it 'included in question object' do
+          expect(comments_response.size).to eq 5
+        end
+
+        %w(id body created_at updated_at).each do |attribute|
+          it "contain #{attribute}" do
+            expect(comment_response[attribute]).to eq comment.send(attribute).as_json
+          end
+        end
+      end
+
+      context 'attachments' do
+        let(:attachment) { attachments.first }
+        let(:attachments_response) { answer_response['attachments'] }
+        let(:attachment_response) { attachments_response.first }
+
+        it 'included in answer object' do
+          expect(attachments_response.size).to eq 4
+        end
+
+        %w(id created_at updated_at).each do |attribute|
+          it "contain #{attribute}" do
+            expect(attachment_response[attribute]).to eq attachment.send(attribute).as_json
+          end
+        end
+
+        it "contain links" do
+          expect(attachment_response['file']['url']).to eq attachment.file.url.as_json
+        end
+      end
+    end
+  end
 end
