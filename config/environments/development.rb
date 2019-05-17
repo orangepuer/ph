5db0d1822b31ab041323bb41b4d43a8268006858
orangepuer <rkhpuer@gmail.com>
2019-05-17 11:46:24 +0300

    Add caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,9 +22,10 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+    
+    #config.cache_store = :null_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
